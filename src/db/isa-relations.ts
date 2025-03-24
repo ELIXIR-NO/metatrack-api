f4c77a5998d578ce4f17918cad2cd527ebc6ecbd
Joshua Baskaran <joshbaskaran@gmail.com>
2025-03-25 00:37:13 +0100

    feat: create all database tables and their relations

---
+++ src/db/isa-relations.ts
@@ -0,0 +1,308 @@
+import { Many, relations } from "drizzle-orm";
+import {
+	ontologySources,
+	ontologyAnnotations,
+	investigations,
+	studies,
+	assays,
+	factors,
+	factorValues,
+	materialAttributes,
+	materialAttributeValues,
+	materials,
+	materialDerivations,
+	people,
+	processParameterValues,
+	protocolParameters,
+	processes,
+	protocols,
+	processInputs,
+	data,
+	processOutputs,
+	protocolComponents,
+	publications,
+	samples,
+	sources,
+	studyMaterials,
+	assayMaterials,
+} from "./schema";
+
+export const investigationsRelations = relations(
+	investigations,
+	({ many }) => ({
+		ontologySourceReferences: many(ontologySources),
+		publications: many(publications),
+		people: many(people),
+		studies: many(studies),
+	}),
+);
+
+export const studyRelations = relations(studies, ({ one, many }) => ({
+	investigation: one(investigations, {
+		fields: [studies.investigation],
+		references: [investigations.id],
+	}),
+	publications: many(publications),
+	people: many(people),
+	studyDesignDescriptors: many(ontologyAnnotations),
+	protocols: many(protocols),
+	processSequence: many(processes),
+	assays: many(assays),
+	factors: many(factors),
+	characteristicCategories: many(materialAttributes),
+	unitCategories: many(ontologyAnnotations),
+	materials: one(studyMaterials, {
+		fields: [studies.materials],
+		references: [studyMaterials.id],
+	}),
+}));
+
+export const studyMaterialRelations = relations(studyMaterials, ({ many }) => ({
+	sources: many(sources),
+	samples: many(samples),
+	materials: many(materials),
+}));
+
+export const assayRelations = relations(assays, ({ one, many }) => ({
+	study: one(studies, {
+		fields: [assays.study],
+		references: [studies.id],
+	}),
+	dataFiles: many(data),
+	characteristicCategories: one(materialAttributes, {
+		fields: [assays.characteristicCategories],
+		references: [materialAttributes.id],
+	}),
+	measurementType: one(ontologyAnnotations, {
+		fields: [assays.measurementType],
+		references: [ontologyAnnotations.id],
+	}),
+	technologyType: one(ontologyAnnotations, {
+		fields: [assays.technologyType],
+		references: [ontologyAnnotations.id],
+	}),
+	unitCategories: one(ontologyAnnotations, {
+		fields: [assays.unitCategories],
+		references: [ontologyAnnotations.id],
+	}),
+	processSequence: many(processes),
+	materials: one(assayMaterials, {
+		fields: [assays.materials],
+		references: [assayMaterials.id],
+	}),
+}));
+
+export const assayMaterialRelations = relations(assayMaterials, ({ many }) => ({
+	samples: many(samples),
+	materials: many(materials),
+}));
+
+export const factorRelations = relations(factors, ({ one }) => ({
+	factoryType: one(ontologyAnnotations, {
+		fields: [factors.factorType],
+		references: [ontologyAnnotations.id],
+	}),
+}));
+
+export const factorValueRelations = relations(factorValues, ({ one }) => ({
+	category: one(ontologyAnnotations, {
+		fields: [factorValues.category],
+		references: [ontologyAnnotations.id],
+	}),
+	ontologyValue: one(ontologyAnnotations, {
+		fields: [factorValues.ontologyValue],
+		references: [ontologyAnnotations.id],
+	}),
+	unit: one(ontologyAnnotations, {
+		fields: [factorValues.unit],
+		references: [ontologyAnnotations.id],
+	}),
+}));
+
+export const materialAttributeRelations = relations(
+	materialAttributes,
+	({ one }) => ({
+		characteristicType: one(ontologyAnnotations, {
+			fields: [materialAttributes.characteristicType],
+			references: [ontologyAnnotations.id],
+		}),
+	}),
+);
+
+export const materialAttributeValueRelations = relations(
+	materialAttributeValues,
+	({ one }) => ({
+		category: one(materialAttributes, {
+			fields: [materialAttributeValues.category],
+			references: [materialAttributes.id],
+		}),
+		ontologyValue: one(ontologyAnnotations, {
+			fields: [materialAttributeValues.ontologyValue],
+			references: [ontologyAnnotations.id],
+		}),
+		unit: one(ontologyAnnotations, {
+			fields: [materialAttributeValues.unit],
+			references: [ontologyAnnotations.id],
+		}),
+	}),
+);
+
+export const materialRelations = relations(materials, ({ many }) => ({
+	characteristics: many(materialAttributeValues),
+	derivesFrom: many(materialDerivations),
+}));
+
+export const materialDerivationRelations = relations(
+	materialDerivations,
+	({ one }) => ({
+		materialId: one(materials, {
+			fields: [materialDerivations.materialId],
+			references: [materials.id],
+		}),
+		derivedFromId: one(materials, {
+			fields: [materialDerivations.derivedFromId],
+			references: [materials.id],
+		}),
+	}),
+);
+
+export const ontologyAnnotationRelations = relations(
+	ontologyAnnotations,
+	({ one }) => ({
+		termSource: one(ontologySources, {
+			fields: [ontologyAnnotations.termSource],
+			references: [ontologySources.id],
+		}),
+	}),
+);
+
+export const ontologySourceRelations = relations(
+	ontologySources,
+	({ one, many }) => ({
+		annotations: many(ontologyAnnotations),
+		investigation: one(investigations, {
+			fields: [ontologySources.investigation],
+			references: [investigations.id],
+		}),
+	}),
+);
+
+export const peopleRelations = relations(people, ({ many }) => ({
+	roles: many(ontologyAnnotations),
+}));
+
+export const processParameterValueRelations = relations(
+	processParameterValues,
+	({ one }) => ({
+		category: one(protocolParameters, {
+			fields: [processParameterValues.category],
+			references: [protocolParameters.id],
+		}),
+		ontologyValue: one(ontologyAnnotations, {
+			fields: [processParameterValues.ontologyValue],
+			references: [ontologyAnnotations.id],
+		}),
+		unit: one(ontologyAnnotations, {
+			fields: [processParameterValues.unit],
+			references: [ontologyAnnotations.id],
+		}),
+	}),
+);
+
+export const processRelations = relations(processes, ({ one, many }) => ({
+	executesProtocol: one(protocols, {
+		fields: [processes.executesProtocol],
+		references: [protocols.id],
+	}),
+	parameterValues: many(processParameterValues),
+	previousProcess: one(processes, {
+		fields: [processes.previousProcess],
+		references: [processes.id],
+	}),
+	nextProcess: one(processes, {
+		fields: [processes.nextProcess],
+		references: [processes.id],
+	}),
+	inputs: many(processInputs),
+	outputs: many(processOutputs),
+}));
+
+export const processInputRelations = relations(processInputs, ({ one }) => ({
+	dataInput: one(data, {
+		fields: [processInputs.dataInput],
+		references: [data.id],
+	}),
+	materialInput: one(materials, {
+		fields: [processInputs.materialInput],
+		references: [materials.id],
+	}),
+	sourceInput: one(sources, {
+		fields: [processInputs.sourceInput],
+		references: [sources.id],
+	}),
+	sampleInput: one(samples, {
+		fields: [processInputs.sampleinput],
+		references: [samples.id],
+	}),
+}));
+
+export const processOutputRelations = relations(processOutputs, ({ one }) => ({
+	dataOutput: one(data, {
+		fields: [processOutputs.dataOutput],
+		references: [data.id],
+	}),
+	materialOutput: one(materials, {
+		fields: [processOutputs.materialOutput],
+		references: [materials.id],
+	}),
+	sampleOutput: one(samples, {
+		fields: [processOutputs.sampleOutput],
+		references: [samples.id],
+	}),
+}));
+
+export const protocolParameterRelations = relations(
+	protocolParameters,
+	({ one }) => ({
+		parameterName: one(ontologyAnnotations, {
+			fields: [protocolParameters.parameterName],
+			references: [ontologyAnnotations.id],
+		}),
+	}),
+);
+
+export const protocolRelations = relations(protocols, ({ one, many }) => ({
+	protocolType: one(ontologyAnnotations, {
+		fields: [protocols.protocolType],
+		references: [ontologyAnnotations.id],
+	}),
+	parameters: many(protocolParameters),
+	components: many(protocolComponents),
+}));
+
+export const protocolComponentsRelations = relations(
+	protocolComponents,
+	({ one }) => ({
+		componentType: one(ontologyAnnotations, {
+			fields: [protocolComponents.componentType],
+			references: [ontologyAnnotations.id],
+		}),
+	}),
+);
+
+export const publicationRelations = relations(publications, ({ one }) => ({
+	status: one(ontologyAnnotations, {
+		fields: [publications.status],
+		references: [ontologyAnnotations.id],
+	}),
+}));
+
+export const sampleRelations = relations(samples, ({ many }) => ({
+	characteristics: many(materialAttributeValues),
+	factorValues: many(factorValues),
+	derivesFrom: many(sources),
+}));
+
+export const sourceRelations = relations(sources, ({ many }) => ({
+	characteristics: many(materialAttributeValues),
+}));
---
+++ src/db/schema/isa-schema.ts
@@ -0,0 +1,298 @@
+import {
+	date,
+	pgEnum,
+	pgTable,
+	text,
+	timestamp,
+	uuid,
+	numeric,
+	primaryKey,
+} from "drizzle-orm/pg-core";
+
+export const investigations = pgTable("investigation", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	identifier: text().notNull().unique(),
+	title: text(),
+	filename: text(),
+	description: text(),
+	submissionDate: date(),
+	publicReleaseDate: date(),
+});
+
+export const studies = pgTable("study", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	identifier: text().notNull().unique(),
+	title: text(),
+	filename: text(),
+	description: text(),
+	submissionDate: date(),
+	publicReleaseDate: date(),
+	materials: uuid(),
+	investigation: uuid()
+		.notNull()
+		.references(() => investigations.id, { onDelete: "cascade" }),
+});
+
+export const studyMaterials = pgTable("study_material", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const assays = pgTable("assay", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	filename: text(),
+	study: uuid()
+		.notNull()
+		.references(() => studies.id, { onDelete: "cascade" }),
+	technologyPlatform: text(),
+	materials: uuid(),
+	characteristicCategories: uuid(),
+	measurementType: uuid(),
+	technologyType: uuid(),
+	unitCategories: uuid(),
+});
+
+export const assayMaterials = pgTable("assay_material", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const dataTypeEnum = pgEnum("dataType", [
+	"Raw Data File",
+	"Derived Data File",
+	"Image File",
+]);
+
+export const data = pgTable("data", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text(),
+	dataType: dataTypeEnum(),
+});
+
+export const factors = pgTable("factor", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	factorName: text(),
+	factorType: uuid(),
+});
+
+export const valueTypeEnum = pgEnum("factor_value_type", [
+	"ontologyAnnotation",
+	"string",
+	"number",
+]);
+
+export const factorValues = pgTable("factor_value", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	category: uuid(),
+	factorValueType: valueTypeEnum(),
+	ontologyValue: uuid(),
+	stringValue: text(),
+	numValue: numeric(),
+	unit: uuid(),
+});
+
+export const materialAttributes = pgTable("material_attribute", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	characteristicType: uuid(),
+});
+
+export const materialAttributeValues = pgTable("material_attribute_value", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	category: uuid(),
+	valueType: valueTypeEnum(),
+	ontologyValue: uuid(),
+	stringValue: text(),
+	numValue: numeric(),
+	unit: uuid(),
+});
+
+export const materialTypeEnum = pgEnum("material_type", [
+	"Extract Name",
+	"Labeled Extract Name",
+]);
+
+export const materials = pgTable("material", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text(),
+	type: materialTypeEnum(),
+});
+
+export const materialDerivations = pgTable(
+	"material_derivations",
+	{
+		materialId: uuid().references(() => materials.id, {
+			onDelete: "cascade",
+		}),
+		derivedFromId: uuid().references(() => materials.id, {
+			onDelete: "cascade",
+		}),
+	},
+	(t) => [primaryKey({ columns: [t.materialId, t.derivedFromId] })],
+);
+
+export const ontologyAnnotations = pgTable("ontology_annotation", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	annotationValue: text(),
+	termSource: uuid()
+		.notNull()
+		.references(() => ontologySources.id, { onDelete: "cascade" }),
+	termAccession: text(),
+});
+
+export const ontologySources = pgTable("ontology_source", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text().notNull(),
+	description: text(),
+	file: text(),
+	version: text(),
+	investigation: uuid()
+		.notNull()
+		.references(() => investigations.id, { onDelete: "cascade" }),
+});
+
+export const people = pgTable("person", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	firstName: text(),
+	lastName: text(),
+	midInitials: text(),
+	email: text(),
+	phone: text(),
+	fax: text(),
+	address: text(),
+	affiliation: text(),
+});
+
+export const processParameterValues = pgTable("process_parameter_value", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	category: uuid(),
+	valueType: valueTypeEnum(),
+	ontologyValue: uuid(),
+	stringValue: text(),
+	numValue: text(),
+	unit: uuid(),
+});
+
+export const processes = pgTable("process", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text(),
+	executesProtocol: uuid(),
+	performer: text(),
+	date: date(),
+	previousProcess: uuid(),
+	nextProcess: uuid(),
+});
+
+export const processInputTypeEnum = pgEnum("process_input_type", [
+	"source",
+	"sample",
+	"data",
+	"material",
+]);
+
+export const processInputs = pgTable("process_input", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	inputType: processInputTypeEnum(),
+	sourceInput: uuid(),
+	sampleinput: uuid(),
+	dataInput: uuid(),
+	materialInput: uuid(),
+});
+
+export const processOutputTypeEnum = pgEnum("process_output_type", [
+	"sampele",
+	"data",
+	"material",
+]);
+
+export const processOutputs = pgTable("process_output", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	outputType: processOutputTypeEnum(),
+	sampleOutput: uuid(),
+	dataOutput: uuid(),
+	materialOutput: uuid(),
+});
+
+export const protocolParameters = pgTable("protocol_parameter", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	parameterName: uuid(),
+});
+
+export const protocols = pgTable("protocol", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text(),
+	protocolType: uuid(),
+	description: text(),
+	uri: text(),
+	version: text(),
+});
+
+export const protocolComponents = pgTable("protocol_component", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	componentName: text(),
+	componentType: uuid(),
+});
+
+export const publications = pgTable("publication", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	pubMedID: text(),
+	doi: text(),
+	authorList: text(),
+	title: text(),
+	status: uuid(),
+});
+
+export const samples = pgTable("sample", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text(),
+});
+
+export const sources = pgTable("source", {
+	id: uuid().defaultRandom().primaryKey(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	name: text(),
+});
