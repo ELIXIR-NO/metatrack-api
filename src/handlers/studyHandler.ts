a9af9f5c25b48da69c4189c6f60f7f211671b15c
Joshua Baskaran <joshbaskaran@gmail.com>
2025-04-08 13:56:10 +0200

    feat: Add CRUD for study

--- src/handlers/studyHandler.ts
+++ src/handlers/studyHandler.ts
@@ -1,11 +1,113 @@
 import Elysia from "elysia";
+import { auth } from "../utils/auth";
+import { getUserRole } from "../lib/projectHelpers";
+import {
+	CreateStudy,
+	createStudy,
+	EditStudy,
+	getAllStudies,
+	getStudyById,
+	updateStudy,
+} from "../lib/studyHelpers";
 
 export const studyHandler = new Elysia({
 	prefix: "/projects/:projectId/investigations/:investigationId/studies",
 	tags: ["study"],
 })
-	.get("/", async () => {})
-	.get("/:studyId", async () => {})
-	.post("/", async () => {})
-	.put("/:studyId", async () => {})
+	.get(
+		"/",
+		async ({ params: { projectId, investigationId }, error, request }) => {
+			const session = await auth.api.getSession({ headers: request.headers });
+			if (!session?.session) return error(401, "Unauthorized");
+
+			const userRole = await getUserRole(projectId, session.user.id);
+			if (!userRole || userRole === "pending") return error(403, "Forbidden");
+
+			try {
+				const studies = await getAllStudies(investigationId);
+				if (!studies) return error(404, "Not Found");
+
+				return studies;
+			} catch {
+				return error(500, "Internal Server Error");
+			}
+		},
+	)
+	.get(
+		"/:studyId",
+		async ({
+			params: { projectId, investigationId, studyId },
+			error,
+			request,
+		}) => {
+			const session = await auth.api.getSession({ headers: request.headers });
+			if (!session?.session) return error(401, "Unauthorized");
+
+			const userRole = await getUserRole(projectId, session.user.id);
+			if (!userRole || userRole === "pending") return error(403, "Forbidden");
+
+			try {
+				const study = await getStudyById(studyId);
+				if (!study) return error(404, "Not Found");
+
+				return study;
+			} catch {
+				return error(500, "Internal Server Error");
+			}
+		},
+	)
+	.post(
+		"/",
+		async ({
+			params: { projectId, investigationId },
+			error,
+			request,
+			body,
+			set,
+		}) => {
+			const session = await auth.api.getSession({ headers: request.headers });
+			if (!session?.session) return error(401, "Unauthorized");
+
+			const userRole = await getUserRole(projectId, session.user.id);
+			if (!userRole || userRole === "pending" || userRole === "reader")
+				return error(403, "Forbidden");
+
+			try {
+				await createStudy(body, investigationId);
+				set.status = 204;
+			} catch {
+				return error(500, "Internal Server Error");
+			}
+		},
+		{
+			body: CreateStudy,
+		},
+	)
+	.put(
+		"/:studyId",
+		async ({
+			params: { projectId, investigationId, studyId },
+			error,
+			request,
+			body,
+			set,
+		}) => {
+			const session = await auth.api.getSession({ headers: request.headers });
+			if (!session?.session) return error(401, "Unauthorized");
+
+			const userRole = await getUserRole(projectId, session.user.id);
+			if (!userRole || userRole === "pending" || userRole === "reader")
+				return error(403, "Forbidden");
+
+			try {
+				await updateStudy(body, investigationId, studyId);
+				set.status = 204;
+			} catch {
+				return error(500, "Internal Server Error");
+			}
+		},
+		{
+			body: EditStudy,
+		},
+	)
 	.delete("/:studyId", async () => {});
---
+++ src/lib/studyHelpers.ts
@@ -0,0 +1,77 @@
+import { and, eq } from "drizzle-orm";
+import { db } from "../db";
+import { studies } from "../db/schema";
+import { Static, t } from "elysia";
+
+const StudyMaterials = t.Object({
+	samples: t.Optional(t.String()),
+	otherMaterials: t.Optional(t.String()),
+});
+
+export const CreateStudy = t.Object({
+	filename: t.Optional(t.String()),
+	identifier: t.String(),
+	title: t.Optional(t.String()),
+	description: t.Optional(t.String()),
+	submissionDate: t.Optional(t.String()),
+	publicReleaseDate: t.Optional(t.String()),
+	publications: t.Optional(t.Array(t.String())),
+	people: t.Optional(t.Array(t.String())),
+	studyDesignDescriptors: t.Optional(t.Array(t.String())),
+	protocols: t.Optional(t.Array(t.String())),
+	materials: t.Optional(t.String()),
+	processSequence: t.Optional(t.Array(t.String())),
+	assays: t.Optional(t.Array(t.String())),
+	factors: t.Optional(t.Array(t.String())),
+	characteristicCategories: t.Optional(t.Array(t.String())),
+	unitCategories: t.Optional(t.Array(t.String())),
+});
+export type TCreateStudy = Static<typeof CreateStudy>;
+
+export const EditStudy = t.Omit(CreateStudy, ["identifier"]);
+export type TEditStudy = Static<typeof EditStudy>;
+
+export async function createStudy(data: TCreateStudy, investigationId: string) {
+	const currentTime = new Date();
+
+	return await db
+		.insert(studies)
+		.values({
+			...data,
+			createdAt: currentTime,
+			updatedAt: currentTime,
+			investigation: investigationId,
+		})
+		.returning();
+}
+
+export async function getAllStudies(investigationId: string) {
+	return await db.query.studies.findMany({
+		where: eq(studies.investigation, investigationId),
+	});
+}
+
+export async function getStudyById(studyId: string) {
+	return await db.query.studies.findFirst({
+		where: eq(studies.id, studyId),
+	});
+}
+
+export async function updateStudy(
+	data: TEditStudy,
+	investigationId: string,
+	studyId: string,
+) {
+	const currentTime = new Date();
+
+	await db
+		.update(studies)
+		.set({ ...data, updatedAt: currentTime })
+		.where(
+			and(eq(studies.id, studyId), eq(studies.investigation, investigationId)),
+		);
+}
+
+export async function deleteStudy(studyId: string) {
+	await db.delete(studies).where(eq(studies.id, studyId));
+}
