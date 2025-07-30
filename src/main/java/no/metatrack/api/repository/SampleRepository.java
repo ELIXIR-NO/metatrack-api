afa25169bc700111459b96c8c2d31560fe1d04b8
Joshua Baskaran <joshbaskaran@gmail.com>
2025-07-30 11:11:37 +0200

    fix: correct parameter annotation in `deleteByAndChildNodesBySampleId` method
    Adds `@Param` annotation to the method parameter and aligns query variable name for proper functionality.

--- src/main/java/no/metatrack/api/repository/SampleRepository.java
+++ src/main/java/no/metatrack/api/repository/SampleRepository.java
@@ -3,6 +3,7 @@ package no.metatrack.api.repository;
 import no.metatrack.api.node.Sample;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,11 +16,11 @@ public interface SampleRepository extends Neo4jRepository<Sample, String> {
 	Optional<Sample> findByName(String name);
 
 	@Query("""
-			MATCH (s:Sample {id: $sampleId})
+			MATCH (s:Sample {id: $id})
 			OPTIONAL MATCH (s)-[:IS_RAW_ATTRIBUTE|HAS_CHARACTERISTIC|HAS_FACTOR_VALUE|DERIVES_FROM]->(child)
 			DETACH DELETE child
 			DETACH DELETE s
 			""")
-	void deleteByAndChildNodesBySampleId(String sampleId);
+	void deleteByAndChildNodesBySampleId(@Param("id") String sampleId);
 
 }
