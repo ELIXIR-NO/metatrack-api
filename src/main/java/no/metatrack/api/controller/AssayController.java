156fab9f9d868802aea449bcf67b0d68951fe599
Joshua Baskaran <joshbaskaran@gmail.com>
2025-08-22 12:04:33 +0200

    feat: add role-based access control to create assay endpoint

--- src/main/java/no/metatrack/api/controller/AssayController.java
+++ src/main/java/no/metatrack/api/controller/AssayController.java
@@ -5,6 +5,7 @@ import no.metatrack.api.dto.AssayResponse;
 import no.metatrack.api.dto.CreateAssayRequest;
 import no.metatrack.api.service.AssayService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,6 +22,7 @@ public class AssayController {
 	}
 
 	@PostMapping
+	@PreAuthorize("@investigationAccess.hasAtLeast(#investigationId, T(no.metatrack.api.enums.InvestigationRole).WRITER)")
 	public ResponseEntity<AssayResponse> createAssay(@PathVariable String investigationId, @PathVariable String studyId,
 			@Valid @RequestBody CreateAssayRequest request) {
 
