30ba670b2dce06bd561a83c9a0b4193de660866e
danilomartinss <danilolmartins@gmail.com>
2025-09-19 12:39:25 +0200

    chore: Updated findAllStudies query
    - Updated query to specify the correlation of variables in studies

--- src/main/java/no/metatrack/api/repository/InvestigationRepository.java
+++ src/main/java/no/metatrack/api/repository/InvestigationRepository.java
@@ -17,7 +17,15 @@ public interface InvestigationRepository extends Neo4jRepository<Investigation, 
 	@Query("match (i:Investigation)-[:HAS_MEMBER]->(u:User {id: $userId}) return i")
 	List<Investigation> findAllByUserId(String userId);
 
-	@Query("match (i:Investigation {id: $investigationId})-[:HAS_STUDY]->(s:Study) return s")
+	@Query("""
+			    MATCH (i:Investigation {id: $investigationId})-[:HAS_STUDY]->(s:Study)
+			    RETURN
+			        s.id AS id,
+			        s.identifier AS identifier,
+			        s.title AS title,
+			        s.description AS description,
+			        s.filename AS filename
+			""")
 	List<Study> findAllStudies(String investigationId);
 
 	@Query("match (i:Investigation {id: $investigationId})-[:HAS_ONTOLOGY]->(o:OntologySourceReference) return o")
